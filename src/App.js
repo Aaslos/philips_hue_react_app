722fa50a7f28173172ba423ae71c0078726d9616
Aaslos <arthur.schrijver@gmail.com>
2022-01-15 09:32:39 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1 className="App-title">A Philips Hue React app</h1>
+                <h1 className="App-title">A Philips Hue React app by Arthur Schrijver</h1>
             </header>
             <LightsView />
         </div>
